477ea87df7a6c11d8f38f5ac971b55c46030ead9
Adeyinka Adebayo A <206746647+PMsteam100@users.noreply.github.com>
2025-06-18 19:45:49 +0100

    new2 commit

--- app.js
+++ app.js
@@ -55,12 +55,12 @@ app.use('/api/auth', authRoutes);
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
-if (!MONGO_URI) {
-  console.error('❌ MONGO_URI is undefined. Set it in Railway Environment Variables.');
-  process.exit(1);
-}
+// if (!MONGO_URI) {
+//   console.error('❌ MONGO_URI is undefined. Set it in Railway Environment Variables.');
+//   process.exit(1);
+// }
 
-mongoose.connect(MONGO_URI, {
+mongoose.connect("mongodb+srv://Adebayo:Gbola51389%40@cluster0.fyd7gun.mongodb.net/user-auth?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
